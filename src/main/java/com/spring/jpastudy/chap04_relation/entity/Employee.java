bdb1bcafbbe2802bf0b07083b85a61b42f252c9a
chanhee7 <chan9505@naver.com>
2024-06-26 19:22:55 +0900

    # 10. JPA 양방향 매핑 기본

--- src/main/java/com/spring/jpastudy/chap04_relation/entity/Employee.java
+++ src/main/java/com/spring/jpastudy/chap04_relation/entity/Employee.java
@@ -25,6 +25,7 @@ public class Employee {
     // 단방향 매핑 - 데이터베이스처럼 한쪽에 상대방의 PK를 FK로 갖는 형태
     // EAGER Loading: 연관된 데이터를 항상 JOIN 을 통해 같이 가져옴
     // LAZY Loading: 해당 엔터티 데이터만 가져오고 필요한 경우 연관엔터티를 가져옴
+    // @ManyToOne 의 기본값 EAGER
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dept_id") // FK 컬럼명
     private Department department;
